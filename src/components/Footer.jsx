a4bd10f0924765df5c996010dc45a726b50f4245
Parteek2023 <72809503+Parteek2023@users.noreply.github.com>
2025-07-31 15:49:58 +0530

    Update Footer.jsx
    just removed few unnecessary things

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer>
 	  <div>
 		<p>Copyright ⓒ {year} Parteek</p>
-        <p>Made with <i class="fas fa-heart colored"></i>in Uttar Pradesh </p>
+       
         <a style={{ textDecoration: 'none' }} href="" class="text-dark link"><p></p></a>
       </div>		
     </footer>
